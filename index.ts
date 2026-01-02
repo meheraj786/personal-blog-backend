3218ff8f52d4418361c661abf05e60aa37501a6f
Meheraj H. <124564659+meheraj786@users.noreply.github.com>
2026-01-02 11:04:52 +0600

    upgrade

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ dotenv.config();
 import express, { Application } from "express";
 import routers from "./routes/index";
 import { dbConnect } from "./database/db.config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +15,8 @@ console.log();
 (async () => {
   try {
     await dbConnect();
+    app.use(cors());
+    app.use(cookieParser());
     app.use(express.json());
     app.use(routers);
 
