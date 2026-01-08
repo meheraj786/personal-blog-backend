6b91062eb41260696b608a99ea652f8c52b0d9d8
Meheraj H. <124564659+meheraj786@users.noreply.github.com>
2026-01-08 19:09:09 +0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import dotenv from "dotenv";
-
 dotenv.config();
-
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { type Application } from "express";
@@ -11,29 +9,31 @@ import { seedDatabase } from "./utils/seeder";
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
+
 console.log();
 
 (async () => {
-	try {
-		await dbConnect();
-
-		app.use(
-			cors({
-				origin: process.env.ORIGIN || "http://localhost:3000",
-				credentials: true,
-			}),
-		);
-		app.use(cookieParser());
-		app.use(express.json());
-		app.use(routers);
-
-		app.get("/", (req, res) => {
-			res.send("Hello World!!");
-		});
-
-		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-		seedDatabase();
-	} catch (error) {
-		console.error("Something went wrong:", error);
-	}
+  try {
+    await dbConnect();
+    app.use(
+      cors({
+        origin: "*",  
+        credentials: true, 
+      }),
+    );
+    
+    app.use(cookieParser());
+    app.use(express.json());
+    app.use(routers);
+    
+    app.get("/", (req, res) => {
+      res.send("Hello World!!");
+    });
+    
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    
+    seedDatabase();
+  } catch (error) {
+    console.error("Something went wrong:", error);
+  }
 })();
